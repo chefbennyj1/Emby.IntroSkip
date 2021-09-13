fc9ad9e6831bd6ddf383ac69a3fcf4f4d7200a5c
chefbennyj1 <chefbennyj@gmail.com>
2021-09-13 10:28:28 -0400

    Ops fixes

--- IntroSkip/Chapters/ChapterEditScheduledTask.cs
+++ IntroSkip/Chapters/ChapterEditScheduledTask.cs
@@ -3,13 +3,8 @@ using MediaBrowser.Controller.Persistence;
 using MediaBrowser.Model.Tasks;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
-using IntroSkip.TitleSequence;
-using MediaBrowser.Model.Querying;
-using IntroSkip.Data;
 
 namespace IntroSkip.Chapters
 {
@@ -17,35 +12,28 @@ namespace IntroSkip.Chapters
     {
         public ILibraryManager LibraryManager {get; set;}
         public IItemRepository ItemRepository;
-        private ITaskManager TaskManager { get;}
+        //private ITitleSequenceRepository Repo;
+        //private ILogger Log;
 
-        public ChapterEditScheduledTask(ILibraryManager libraryManager, IItemRepository itemRepo, ITaskManager taskManager)
+        public ChapterEditScheduledTask(ILibraryManager libraryManager, IItemRepository itemRepo /*ITitleSequenceRepository repo ILogger log*/)
         {
             LibraryManager = libraryManager;
             ItemRepository = itemRepo;
-            TaskManager = taskManager;
+            //Repo = repo;
+            //Log = log;
         }
+        //QueryResult<TitleSequenceResult> dbResults = null;
         
-        public Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
+        public async Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
         {
-            //Run the ChapterEdit task and wait for it to finish before moving on to Image extraction
-            Task chapterExecute = Task.Factory.StartNew(ProcessEpisodeChaptersPoints, cancellationToken);
-            chapterExecute.Wait(cancellationToken);
-
-            var config = Plugin.Instance.Configuration;
-            
-            // If the user has enabled Chapter Image Extraction in the Advanced menu then lets run that process! 
-            if (chapterExecute.IsCompleted && config.EnableAutomaticImageExtraction == true)
-            {
-                ProcessChapterImageExtraction();
-            }
-            //we need to return the Chapter Point Edit Task to close out that the task has completed otherwise the process flags as "Failed"
-            return chapterExecute;
+            ProcessEpisodeChaptersPoints();
+            //ChapterManager.Instance.EditChapters(11951);
         }
 
+        
         public bool IsHidden => false;
 
-        public bool IsEnabled => false;
+        public bool IsEnabled => true;
 
         public bool IsLogged => true;
 
@@ -56,19 +44,24 @@ namespace IntroSkip.Chapters
         public string Description => "Insert a Chapter Marker for Intro Start and End Times";
 
         public string Category => "Intro Skip";
-                
+
+        
 
         public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
         {
             return new[]
             {
-                new TaskTriggerInfo()                
+                new TaskTriggerInfo
+                {
+                    Type          = TaskTriggerInfo.TriggerInterval,
+                    IntervalTicks = TimeSpan.FromHours(24).Ticks
+                }                
             };
         }
 
-        public Task ProcessEpisodeChaptersPoints()
+        public void ProcessEpisodeChaptersPoints()
         {
-            
+            ILogger Log;
             QueryResult<TitleSequenceResult> dbResults = null;
             ITitleSequenceRepository repo = IntroSkipPluginEntryPoint.Instance.Repository;
             dbResults = repo.GetResults(new TitleSequenceResultQuery());
@@ -78,21 +71,11 @@ namespace IntroSkip.Chapters
                 if(episode.HasSequence)
                 {
                     var id = episode.InternalId;
+                    //Log.Info("CHAPTER EDIT: TASK ---- ID = {0}", id);
                     ChapterManager.Instance.EditChapters(id);
+
                 }
             }
-
-            return null;
-        }
-
-        public Task ProcessChapterImageExtraction()
-        {
-            //var chapterEdit = TaskManager.ScheduledTasks.FirstOrDefault(task => task.Name == "IntroSkip Chapter Insertion");
-            var thumbnail = TaskManager.ScheduledTasks.FirstOrDefault(task => task.Name == "Thumbnail image extraction");
-            
-            TaskManager.Execute(thumbnail, new TaskOptions());
-
-            return null;
         }
     }
 }
