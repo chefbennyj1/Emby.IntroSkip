4af49762fb600ecffa483c5f4ae469bee1de0aef
chefbennyj1 <chefbennyj@gmail.com>
2021-09-18 13:28:34 -0400

    TitleSEquenceChapterInsertionService

---
+++ IntroSkip/Api/TitleSequenceChapterInsertionService.cs
@@ -0,0 +1,29 @@
+﻿using IntroSkip.Chapters;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Model.Services;
+
+namespace IntroSkip.Api
+{
+    //Our chapter service. This will be available to the UI, or anyone else who wants to access this data.
+    //It will be in the swagger as well. So it will be possible to test these endpoints.
+    public class TitleSequenceChapterInsertionService : IService
+    {
+        private IJsonSerializer JsonSerializer { get; }
+        public TitleSequenceChapterInsertionService(IJsonSerializer json)
+        {
+            JsonSerializer = json;
+        }
+
+        
+        [Route("/ChapterErrors", "GET", Summary = "List of chapters that had issues during the ChapterEditScheduledTask. Available for the life of the Server only.")]
+        public class ChapterErrorRequest : IReturn<string>
+        {
+            
+        }
+
+        public string Get(ChapterErrorRequest request)
+        {
+            return JsonSerializer.SerializeToString(ChapterInsertion.Instance.ChapterErrors);
+        }
+    }
+}
---
+++ IntroSkip/Chapters/ChapterError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroSkip.Chapters
+{
+    public class ChapterError
+    {
+        public long Id { get; set; }
+        public int ChapterCount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
