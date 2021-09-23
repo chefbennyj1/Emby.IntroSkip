bf828ea8ac23059c4acb5142562384ff781a2170
chefbennyj1 <chefbennyj@gmail.com>
2021-09-22 23:38:21 -0400

    Sync items added and removed

---
+++ IntroSkip/AudioFingerprinting/AudioFingerprintManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.MediaEncoding;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Logging;
+
+namespace IntroSkip.AudioFingerprinting
+{
+    public class AudioFingerprintManager : IServerEntryPoint
+    {
+        public static AudioFingerprintManager Instance { get; set; }
+        private IFileSystem FileSystem { get; }
+        private IFfmpegManager FfmpegManager { get; }
+        private ILogger Log { get; }
+
+        public AudioFingerprintManager(IFileSystem file, IFfmpegManager ffmpeg, ILogManager logManager)
+        {
+            Instance = this;
+            FileSystem = file;
+            FfmpegManager = ffmpeg;
+            Log = logManager.GetLogger(Plugin.Instance.Name);
+        }
+
+        public List<uint> GetAudioFingerprint(BaseItem episode, CancellationToken cancellationToken, int duration)
+        {
+            var separator = FileSystem.DirectorySeparatorChar;
+            var fingerprintBinFileName = $"{episode.Parent.InternalId} - {episode.InternalId}.bin";
+            var fingerprintBinFilePath = $"{AudioFingerprintFileManager.Instance.GetEncodingDirectory()}{separator}{fingerprintBinFileName}";
+
+            ExtractFingerprintBinaryData($"{episode.Path}", fingerprintBinFilePath, duration, cancellationToken);
+
+            Task.Delay(300, cancellationToken); //Give enough time for ffmpeg to save the file.
+
+            return SplitByteData($"{fingerprintBinFilePath}", episode);
+        }
+
+        private void ExtractFingerprintBinaryData(string input, string output, int duration, CancellationToken cancellationToken, string titleSequenceStart = "00:00:00")
+        {
+            var ffmpegConfiguration = FfmpegManager.FfmpegConfiguration;
+            var ffmpegPath = ffmpegConfiguration.EncoderPath;
+
+            var args = new[]
+            {
+                $"-ss {titleSequenceStart}",
+                $"-t 00:{duration}:00",
+                $"-i \"{input}\"",
+                "-ac 1",
+                "-acodec pcm_s16le", //What happens if we encode mono AAC?
+                "-ar 16000", //11025
+                "-c:v nul",
+                "-f chromaprint",
+                "-fp_format raw",
+                $"\"{output}\""
+            };
+
+            var procStartInfo = new ProcessStartInfo(ffmpegPath, string.Join(" ", args))
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            using (var process = new Process { StartInfo = procStartInfo })
+            {
+                process.Start();
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //
+                // Very Important!
+                // Leave the standardError.ReadLine in place for this process,
+                // do not remove it,
+                // or the process will kickoff, and the task will apparently complete before any of the encodings are finished,
+                // resulting in no fingerprinted audio!!
+                //
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+                // ReSharper disable once NotAccessedVariable <-- Resharper is incorrect. It is being used
+                string processOutput = null;
+
+                while ((processOutput = process.StandardError.ReadLine()) != null)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        try
+                        {
+                            process.Kill(); //At least try and kill ffmpeg.
+                        }
+                        catch { }
+                    }
+                    //Log.Info(processOutput);
+                }
+
+                //Log.Info($"Chroma-print binary extraction successful: { input }");
+
+            }
+        }
+
+        private List<uint> SplitByteData(string bin, BaseItem item)
+        {
+            Log.Debug($"{item.Parent.Parent.Name} - S:{item.Parent.IndexNumber} - E:{item.IndexNumber}: Extracting chunks from binary chroma-print.");
+            if (!FileSystem.FileExists(bin))
+            {
+                Log.Warn($"{item.Parent.Parent.Name} - S:{item.Parent.IndexNumber} - E:{item.IndexNumber} .bin file doesn't exist.");
+                throw new Exception("bin file doesn't exist");
+            }
+            var fingerprint = new List<uint>();
+            using (BinaryReader b = new BinaryReader(File.Open(bin, FileMode.Open)))
+            {
+                int length = (int)b.BaseStream.Length / sizeof(uint);
+                for (int i = 0; i < length; i++)
+                {
+                    fingerprint.Add(b.ReadUInt32());
+                }
+            }
+            AudioFingerprintFileManager.Instance.RemoveEpisodeFingerprintBinFile(bin, item);
+            return fingerprint;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
--- IntroSkip/IntroSkipPluginEntryPoint.cs
+++ IntroSkip/IntroSkipPluginEntryPoint.cs
@@ -10,6 +10,7 @@ using MediaBrowser.Model.Tasks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace IntroSkip
 {
@@ -18,20 +19,24 @@ namespace IntroSkip
         public static IntroSkipPluginEntryPoint Instance { get; set; }
         private ITitleSequenceRepository Repository { get; set; }
         private ILibraryManager LibraryManager { get; set; }
-        private ITaskManager TaskManager { get; set; }
-        private IServerConfigurationManager Config { get; set; }
-        private ILogger Logger { get; set; }
-        private IJsonSerializer _json { get; set; }
+        private static ITaskManager TaskManager { get; set; }
+        private static IServerConfigurationManager Config { get; set; }
+        private static ILogger Logger { get; set; }
+        private static IJsonSerializer _json { get; set; }
 
-
-        public IntroSkipPluginEntryPoint(ILogger logger, IServerConfigurationManager config, IJsonSerializer json, ILibraryManager libraryManager, ITaskManager taskManager)
+        //Handling new items added to the library
+        private static readonly Timer ItemsAddedTimer = new Timer(AllItemsAdded);
+        private static readonly Timer ItemsRemovedTimer = new Timer(AllItemsRemoved);
+        private static readonly List<long> ItemsRemoved = new List<long>();
+    
+        public IntroSkipPluginEntryPoint(ILogManager logManager, IServerConfigurationManager config, IJsonSerializer json, ILibraryManager libraryManager, ITaskManager taskManager)
         {
-            Logger = logger;
-            _json = json;
-            Config = config;
+            _json          = json;
+            Config         = config;
             LibraryManager = libraryManager;
-            TaskManager = taskManager;
-            Instance = this;
+            TaskManager    = taskManager;
+            Instance       = this;
+            Logger         = logManager.GetLogger(Plugin.Instance.Name);
         }
 
         public void Dispose()
@@ -42,20 +47,10 @@ namespace IntroSkip
 
         public void Run()
         {
-            try
-            {
-                Repository = GetRepository();
-            }
-            catch (Exception ex)
-            {
-                Logger.ErrorException("Error initializing title sequence database", ex);
-            }
-
-            Logger.Info("Database loaded Sucessfully...");
-
+            ItemsAddedTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            ItemsRemovedTimer.Change(Timeout.Infinite, Timeout.Infinite);
             LibraryManager.ItemAdded += LibraryManager_ItemAdded;
             LibraryManager.ItemRemoved += LibraryManager_ItemRemoved;
-
 
             Plugin.Instance.SaveConfiguration();
         }
@@ -63,52 +58,62 @@ namespace IntroSkip
 
         private void LibraryManager_ItemRemoved(object sender, ItemChangeEventArgs e)
         {
+            ItemsRemovedTimer.Change(2000, 2000);
             var item = e.Item;
-            var removableItems = new List<long>();
-            switch (item.GetType().Name)
+            if (item.GetType().Name != "Episode")
             {
-                case "Episode":
-                    {
-                        removableItems.Add(e.Item.InternalId);
-                        break;
-                    }
-                case "Season":
-                    {
-                        removableItems.AddRange(Repository.GetResults(new TitleSequenceResultQuery() { SeasonInternalId = e.Item.InternalId }).Items.Select(i => i.InternalId));
-                        break;
-                    }
-                case "Series":
-                    {
-                        removableItems.AddRange(Repository.GetResults(new TitleSequenceResultQuery()).Items.Where(i => i.SeriesId == item.InternalId).Select(i => i.InternalId));
-                        break;
-                    }
+                return;
             }
-
-            foreach (var removableItem in removableItems)
-            {
-                Repository.Delete(removableItem.ToString());
-            }
-            Repository.Vacuum();
+            ItemsRemoved.Add(e.Item.InternalId); //Add the removed Item to the list of items being removed.
         }
 
-        private async void LibraryManager_ItemAdded(object sender, ItemChangeEventArgs e)
+        
+        private void LibraryManager_ItemAdded(object sender, ItemChangeEventArgs e)
         {
-            if (e.UpdateReason != ItemUpdateType.MetadataDownload || e.UpdateReason != ItemUpdateType.None)
+            if (e.Item.GetType().Name != "Episode")
             {
                 return;
             }
 
-            var fingerprint = TaskManager.ScheduledTasks.FirstOrDefault(task => task.Name == "Episode Audio Fingerprinting");
-            if (fingerprint.State != TaskState.Running)
+            //if the timer is reset then a new item has been added
+            //if the timer goes off, then no new items have been added
+            ItemsAddedTimer.Change(2000, 2000); //Reset the timer because we just got a new episode item.
+
+        }
+
+        private static void AllItemsRemoved(object state)
+        {
+            Logger.Info("Items removed from library... syncing database.");
+            ItemsRemovedTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            var repository = Instance.GetRepository();
+            foreach (var item in ItemsRemoved)
             {
                 try
                 {
-                    await TaskManager.Execute(fingerprint, new TaskOptions());
-                }
-                catch (Exception ex)
-                {
-                    Logger.Warn(ex.Message);
-                }
+                    repository.Delete(item.ToString());
+                }catch{}
+            }
+            ItemsRemoved.Clear();
+            repository.Vacuum();
+            var repo = repository as IDisposable;
+            repo?.Dispose();
+        }
+
+        private static async void AllItemsAdded(object state)
+        {
+            ItemsAddedTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            Logger.Info("New Items are ready to fingerprint scan...");
+            var fingerprint = TaskManager.ScheduledTasks.FirstOrDefault(task => task.Name == "Episode Audio Fingerprinting");
+
+            if (fingerprint?.State == TaskState.Running) return;
+
+            try
+            {
+                await TaskManager.Execute(fingerprint, new TaskOptions());
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn(ex.Message);
             }
         }
 
